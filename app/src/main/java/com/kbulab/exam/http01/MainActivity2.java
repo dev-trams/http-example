e51d69469332ed663f68e6b019039db69a292e34
trams <kppcgold@gmail.com>
2023-03-08 13:59:15 +0900

    aslfi

--- app/src/main/java/com/kbulab/exam/http01/MainActivity2.java
+++ app/src/main/java/com/kbulab/exam/http01/MainActivity2.java
@@ -3,6 +3,9 @@ package com.kbulab.exam.http01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        WebView webView = (WebView) findViewById(R.id.webpage1);
+        webView.getSettings().setJavaScriptEnabled(true);
     }
 }
